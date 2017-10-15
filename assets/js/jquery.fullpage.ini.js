eed2eb33d7cd5c08cc4eec7a2c82ebad0f98af77
Pedro Serapio <pedro.serapio@gmail.com>
2017-10-15 18:09:56 +0800

    MeSopa Review
    MeSopa Content review, re-organized and added the cinematography page.

---
+++ assets/js/jquery.fullpage.ini.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips: ['MeSopa', 'Playground Moments', 'Pop Secret', 'Handcrafted Art', 'Danny Bukenov', 'Tasty Bakery', 'Pol Garcia Blades', 'Find Me'],
+    responsiveWidth: 1100
+  });
+});
